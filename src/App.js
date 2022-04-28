3d9239626ea07eb38a7aa38b2cb3ab79d29998df
Shem Bar <shem@svcollege.co.il>
2022-04-28 11:52:30 +0300

    react app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import {useEffect,useState} from 'react'
+import {HashRouter} from 'react-router-dom'
+
+function App() {
+
+
+  return(
+    <div>
+      hello
+      <HashRouter>
+        
+      </HashRouter>
+    </div>
+  )
+
+
+
+}
+
+export default App;
